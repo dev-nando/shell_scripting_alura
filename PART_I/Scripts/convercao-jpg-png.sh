109e3194093ffb8bd79b2d6bfbe0bb297bbcb53c
mac_02 <mac_02@email.com>
2022-01-18 09:38:57 -0300

    Convertendo imagem e montando script

---
+++ PART_I/Scripts/convercao-jpg-png.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+convert ~/Desenvolvimento/cursos/shell_scripting_alura/PART_I/imagens-livros/amazon_aws.jpg ~/Desenvolvimento/cursos/shell_scripting_alura/PART_I/imagens-livros/amazon_aws.png
