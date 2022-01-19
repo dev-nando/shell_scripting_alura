63b0a51880b016aa5e3234ef4441e35150c39a33
mac_02 <mac_02@email.com>
2022-01-19 12:57:32 -0300

    Removendo extensão e salvando em outro diretório

--- PART_I/Scripts/convercao-jpg-png.sh
+++ PART_I/Scripts/convercao-jpg-png.sh
@@ -2,8 +2,14 @@
 
 cd ~/Desenvolvimento/cursos/shell_scripting_alura/PART_I/imagens-livros/
 
+if [ ! -d png ]
+then
+    mkdir png
+fi
+
 for imagem in *.jpg
 do
-	convert $imagem $imagem.png
+	imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+	convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
 done
 
